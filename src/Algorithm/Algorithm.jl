82f60ccd60a5764078eedc241e75b00cb2d7b598
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-01-17 14:54:30 +0000

    Update Algorithm.jl

--- src/Algorithm/Algorithm.jl
+++ src/Algorithm/Algorithm.jl
@@ -6,7 +6,10 @@ algorithms(algorithms.jl), kernels inside an Baytes package (kernel.jl) can be u
 3. Functions shown in both, algorithms.jl and kernel.jl, need to be defined in either case.
 =#
 ############################################################################################
-# Define Abstract Super Types
+# Define Abstract Super Types - so we do not need to load ModelWrappers for abstraction and function definitions
+abstract type AbstractModelWrapper end
+abstract type AbstractObjective end
+abstract type AbstractResult end
 
 ############################################################################################
 # Include files
@@ -16,3 +19,7 @@ include("utility.jl")
 
 ############################################################################################
 # Export
+export
+    AbstractModelWrapper,
+    AbstractObjective,
+    AbstractResult
