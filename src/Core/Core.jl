1b372d1df66d70b3021f915933360cd9238d1a59
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-01-19 16:32:25 +0000

    Fix deps

--- src/Core/Core.jl
+++ src/Core/Core.jl
@@ -4,6 +4,8 @@ include("generated.jl")
 include("helper.jl")
 include("data.jl")
 include("utility.jl")
+include("chains.jl")
+include("jitter.jl")
 include("printing.jl")
 
 ############################################################################################
