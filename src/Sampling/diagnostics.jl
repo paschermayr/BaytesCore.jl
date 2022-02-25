130dfbe268b51569c179835e745fb197d6e99cec
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-02-25 15:56:41 +0000

    Add iteration counter

--- src/Sampling/diagnostics.jl
+++ src/Sampling/diagnostics.jl
@@ -14,12 +14,15 @@ struct BaseDiagnostics{P}
 	temperature::Float64
     "Prediction of future data."
 	prediction::P
+    "Current iteration of kernel."
+    iter::Int64
     function BaseDiagnostics(
         ℓobjective::S,
         temperature::T,
-        prediction::P
+        prediction::P,
+        iter::Integer
     ) where {S<:AbstractFloat, T<:AbstractFloat, P}
-        return new{P}(ℓobjective, temperature, prediction)
+        return new{P}(ℓobjective, temperature, prediction, iter)
     end
 end
 
@@ -39,7 +42,7 @@ Return `ℓobjective` scaled up to temperature == 1.
 
 """
 function untemper(diagnostics::BaseDiagnostics)
-    return 1/diagnostics.temperature*diagnostics.ℓobjective
+    return (1.0/diagnostics.temperature) * diagnostics.ℓobjective
 end
 
 ############################################################################################
