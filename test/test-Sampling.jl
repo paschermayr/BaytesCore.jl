abb2d9c04bde3c17a6bf2affd70d5b506cb1d300
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-03-07 15:41:26 +0000

    Update test-Sampling.jl

--- test/test-Sampling.jl
+++ test/test-Sampling.jl
@@ -40,6 +40,47 @@ end
 # Shuffle
 
 @testset "Sampling - Shuffle: " begin
+    # Parameter
+    shuffle_ndata = 10
+    shuffle_nparticles = 5
+    shuffle_lookback = 2
+    shuffle_ancestors = [5, 4, 4, 3, 1]
+    shuffle_anestorbuffer = zeros(Int64, shuffle_nparticles)
+
+    shuffle_val = repeat(1:shuffle_nparticles, 1, shuffle_ndata)
+    shuffle_valbuffer = zeros(Int64, shuffle_nparticles)
+
+    shuffle_allancestors = repeat(1:shuffle_nparticles, 1, shuffle_ndata)
+    for iter in 1:shuffle_nparticles
+        shuffle_allancestors[iter, end] = shuffle_ancestors[iter]
+    end
+
+
+    # Basic shuffle! for last dimension
+    shuffle_tmp = deepcopy(shuffle_val)
+    shuffle!(shuffle_tmp, shuffle_buffer, shuffle_ancestors)
+    @test sum( vec(sum(shuffle_tmp, dims=1)) .== sum(shuffle_ancestors) ) == shuffle_ndata
+
+    # Shuffle_forward!
+    shuffle_tmp = deepcopy(shuffle_val)
+    shuffle_forward!(
+        shuffle_tmp,
+        shuffle_valbuffer,
+        shuffle_allancestors,
+        shuffle_anestorbuffer,
+        shuffle_lookback, shuffle_ndata
+    )
+    @test shuffle_allancestors[1, shuffle_ndata] == shuffle_allancestors[end, shuffle_ndata-1]
+    @test shuffle_tmp[1, shuffle_ndata] == shuffle_tmp[end, shuffle_ndata-shuffle_lookback]
+
+    # Shuffle backward
+    shuffle_tmp = deepcopy(shuffle_val)
+    shuffle_backward!(
+        shuffle_tmp,
+        shuffle_valbuffer,
+        shuffle_allancestors,
+        shuffle_anestorbuffer
+    )
 
 end
 
@@ -49,6 +90,7 @@ end
 # Statistics
 
 @testset "Sampling - Statistics: " begin
+    accept_stat = AcceptStatistic
 
 end
 
