e1f5f956a69aa9a90b2d6a9651b1058ed21f02e7
Patrick Aschermayr <p.aschermayr@gmail.com>
2023-06-08 11:54:03 +0200

    Update deps

--- src/BaytesCore.jl
+++ src/BaytesCore.jl
@@ -9,7 +9,7 @@ import LogExpFunctions: LogExpFunctions, logsumexp, logaddexp
 using DocStringExtensions:
     DocStringExtensions, TYPEDEF, TYPEDFIELDS, FIELDS, SIGNATURES, FUNCTIONNAME
 using ArgCheck: ArgCheck, @argcheck
-using UnPack: @unpack, @pack!
+using SimpleUnPack: @unpack, @pack!
 using Random: Random, AbstractRNG, GLOBAL_RNG
 using Statistics: Statistics, middle
 
